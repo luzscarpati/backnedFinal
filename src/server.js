b56e90fb9e8ad6a9d6e88fe89a1190da9f622458
Luz Scarpati <luz.scarpati@gmail.com>
2023-12-27 16:20:16 -0300

    Creación de handlebars

--- src/server.js
+++ src/server.js
@@ -3,10 +3,21 @@ import morgan from "morgan";
 import MainRouter from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { initMongoDB } from "./config/connection.js";
+import expressHandlebars from "express-handlebars";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
 import 'dotenv/config';
 
 const mainRouter = new MainRouter();
 const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
