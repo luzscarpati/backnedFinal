60e8777323d2da66c7cc7c3939158b13ab414522
Luz Scarpati <luz.scarpati@gmail.com>
2024-01-23 16:27:17 -0300

    Arreglo de la lógica del cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import productRouter from "./product.router.js";
 import userRouter from "./user.router.js";
+import cartRouter from "./cart.router.js"
 
 export default class MainRouter {
     constructor(){
@@ -11,7 +12,8 @@ export default class MainRouter {
     initRoutes() {
         this.router.use('/products', productRouter);
         this.router.use('/users', userRouter);
-        this.router.use('/carts')
+        this.router.use('/carts', cartRouter);
+        this.router.use('/ticket', ticketRouter);
     };
 
     getRouter() {
--- src/services/cart.services.js
+++ src/services/cart.services.js
@@ -7,4 +7,83 @@ export default class CartService extends Services {
     constructor(){
         super(cartDao);
     };
+
+    async remove (id) {
+        try {
+          const cartDel = await cartDao.delete(id);
+          if (!cartDel) return false;
+          else return cartDel;
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      
+    
+    async addProdToCart (cartId, prodId) {
+      try {
+        const existCart = await getById(cartId);
+        console.log("existCart-->", existCart);
+        if (!existCart) return false;
+    
+        const existProd = await prodDao.getById(prodId);
+        console.log("existProd-->", existProd);
+        if (!existProd) return false;
+          //SI EXISTE, aumenta quantity++
+        const existProdInCart = existCart.products.find((p)=>p.product._id.toString() === prodId.toString());
+        if(existProdInCart) {
+          existProdInCart.quantity++;
+          existCart.save();
+          return existProdInCart;
+        } else {
+          return await cartDao.addProdToCart(existCart, prodId);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    
+    async removeProdToCart (cartId, prodId) {
+        try {
+          const existCart = await getById(cartId);
+          console.log("existCart-->", existCart);
+          if (!existCart) return false;
+      
+          const existProd = existCart.products.find((p)=>p.product._id.toString() === prodId.toString());
+          console.log("existProd-->", existProd);
+          if (!existProd) return false;
+      
+          return await cartDao.removeProdToCart(existCart, existProd);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      async updateProdQuantityToCart (cartId, prodId, quantity) {
+        try {
+          const existCart = await getById(cartId);
+          console.log("existCart-->", existCart);
+          if (!existCart) return false;
+      
+          const existProd = existCart.products.find((p)=>p.product._id.toString() === prodId.toString());
+          console.log("existProd-->", existProd);
+          if (!existProd) return false;
+      
+          return await cartDao.updateProdQuantityToCart(existCart, existProd, quantity);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      async clearCart (cartId) {
+        try {
+          const existCart = await getById(cartId);
+          console.log("existCart-->", existCart);
+          if (!existCart) return false;
+    
+          return await cartDao.clearCart(existCart);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
 };
